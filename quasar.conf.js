8e67043760a787407f52d3b5da2085e257b2de07
hg.park <hg.park@fromthered.com>
2021-01-13 10:51:04 +0900

    라우트모드 env에 추가

--- quasar.conf.js
+++ quasar.conf.js
@@ -49,7 +49,7 @@ module.exports = configure(function (/* ctx */) {
 
         // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
         build: {
-            vueRouterMode: 'hash', // available values: 'hash', 'history'
+            vueRouterMode: process.env.VUE_ROUTER_MODE, // available values: 'hash', 'history'
 
             // transpile: false,
 
